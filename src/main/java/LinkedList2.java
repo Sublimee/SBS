02dd77440a31d3c274b92b06342d280af95caa01
Maksim Gusev <aquariusborelias@gmail.com>
2022-04-18 19:48:27 +0300

    formatted

--- src/main/java/LinkedList2.java
+++ src/main/java/LinkedList2.java
@@ -142,3 +142,15 @@ public class LinkedList2 {
         }
     }
 }
+
+class Node {
+    public int value;
+    public Node next;
+    public Node prev;
+
+    public Node(int _value) {
+        value = _value;
+        next = null;
+        prev = null;
+    }
+}
