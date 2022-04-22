e25c3caf6b4a96f5bfeded3a6afa70fada513ce2
Maksim Gusev <aquariusborelias@gmail.com>
2022-04-22 21:23:05 +0300

    formatted

--- src/main/java/org/example/lesson06/Deque.java
+++ src/main/java/org/example/lesson06/Deque.java
@@ -12,25 +12,21 @@ public class Deque<T> {
 
     public void addFront(T item) {
         list.addFirst(item);
-        // добавление в голову
     }
 
     public void addTail(T item) {
         list.addLast(item);
-        // добавление в хвост
     }
 
     public T removeFront() {
-        // удаление из головы
         return (size() > 0) ? list.removeFirst() : null;
     }
 
     public T removeTail() {
-        // удаление из хвоста
         return (size() > 0) ? list.removeLast() : null;
     }
 
     public int size() {
-        return list.size(); // размер очереди
+        return list.size();
     }
 }
---
+++ src/test/java/lesson06/PalindromeCheckerTest.java
@@ -0,0 +1,56 @@
+package lesson06;
+
+import org.example.lesson06.PalindromeChecker;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class PalindromeCheckerTest {
+
+    @Test
+    public void checkNullTest() {
+        PalindromeChecker checker = new PalindromeChecker();
+        Assertions.assertFalse(checker.check(null));
+    }
+
+    @Test
+    public void checkSingleTest() {
+        PalindromeChecker checker = new PalindromeChecker();
+        Assertions.assertTrue(checker.check("1"));
+    }
+
+    @Test
+    public void checkPalindromePairTest() {
+        PalindromeChecker checker = new PalindromeChecker();
+        Assertions.assertTrue(checker.check("11"));
+    }
+
+    @Test
+    public void checkNotPalindromePairTest() {
+        PalindromeChecker checker = new PalindromeChecker();
+        Assertions.assertFalse(checker.check("21"));
+    }
+
+    @Test
+    public void checkPalindromeTripleTest() {
+        PalindromeChecker checker = new PalindromeChecker();
+        Assertions.assertTrue(checker.check("131"));
+    }
+
+    @Test
+    public void checkNotPalindromeTripleTest() {
+        PalindromeChecker checker = new PalindromeChecker();
+        Assertions.assertFalse(checker.check("132"));
+    }
+
+    @Test
+    public void checkPalindromeTest() {
+        PalindromeChecker checker = new PalindromeChecker();
+        Assertions.assertTrue(checker.check("2332"));
+    }
+
+    @Test
+    public void checkNotPalindromeTest() {
+        PalindromeChecker checker = new PalindromeChecker();
+        Assertions.assertFalse(checker.check("23142"));
+    }
+}
