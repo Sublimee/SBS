74c47ed7b31b5f65eb19ad2261fe96731d1aaa8f
Maksim Gusev <aquariusborelias@gmail.com>
2022-04-24 17:07:11 +0300

    division and subtraction added to OperandStack

--- src/main/java/org/example/lesson04/OperandStack.java
+++ src/main/java/org/example/lesson04/OperandStack.java
@@ -1,31 +1,60 @@
 package org.example.lesson04;
 
+import java.math.BigInteger;
 import java.util.*;
+import java.util.function.BiFunction;
 
 public class OperandStack {
 
-    public Integer process(List<Object> sequence) {
-        Stack<Object> stack1 = new Stack<>();
-        Stack<Object> stack2 = new Stack<>();
+    private Stack<Object> accumulator;
+
+    public BigInteger process(List<Object> sequence) {
+        Stack<Object> operands = new Stack<>();
+        accumulator = new Stack<>();
 
         for (int i = sequence.size() - 1; i >= 0; i--) {
-            stack1.push(sequence.get(i));
+            operands.push(sequence.get(i));
         }
 
         Object value;
-        while ((value = stack1.pop()) != null) {
+        while ((value = operands.pop()) != null) {
             if (value instanceof Number) {
-                stack2.push(value);
+                accumulator.push(BigInteger.valueOf((Integer) value));
             } else {
                 switch ((String) value) {
-                    case "+" -> stack2.push(((Integer) stack2.pop()) + ((Integer) stack2.pop()));
-                    case "*" -> stack2.push(((Integer) stack2.pop()) * ((Integer) stack2.pop()));
+                    case "+" -> sumOperands();
+                    case "*" -> multiplyOperands();
+                    case "/" -> divideOperands();
+                    case "-" -> subtractOperands();
                     case "=" -> {
-                        return (Integer) stack2.pop();
+                        return (BigInteger) accumulator.pop();
                     }
                 }
             }
         }
         return null;
     }
+
+    private void sumOperands() {
+        processOperands(BigInteger::add);
+    }
+
+    private void multiplyOperands() {
+        processOperands(BigInteger::multiply);
+    }
+
+    private void divideOperands() {
+        processOperands(BigInteger::divide);
+    }
+
+    private void subtractOperands() {
+        processOperands(BigInteger::subtract);
+    }
+
+    private void processOperands(BiFunction<BigInteger, BigInteger, BigInteger> operator) {
+        BigInteger secondOperand = (BigInteger) accumulator.pop();
+        BigInteger firstOperand = (BigInteger) accumulator.pop();
+        accumulator.push(operator.apply(firstOperand, secondOperand));
+    }
+
 }
