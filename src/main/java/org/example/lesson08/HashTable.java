5d154f23431d424f28c4b3a619fb990af5a04a80
Maksim Gusev <aquariusborelias@gmail.com>
2022-04-30 11:37:52 +0300

    formatted

--- src/main/java/org/example/lesson08/HashTable.java
+++ src/main/java/org/example/lesson08/HashTable.java
@@ -15,7 +15,7 @@ public class HashTable {
     }
 
     public int hashFun(String value) {
-        return value.hashCode() % size;
+        return Math.abs(value.hashCode()) % size;
     }
 
     public int seekSlot(String value) {
