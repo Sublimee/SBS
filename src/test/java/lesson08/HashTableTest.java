1aa6e90fe609e98422f9fa2132041f1b55238d2c
Maksim Gusev <aquariusborelias@gmail.com>
2022-04-30 12:22:05 +0300

    formatted

--- src/test/java/lesson08/HashTableTest.java
+++ src/test/java/lesson08/HashTableTest.java
@@ -14,7 +14,8 @@ public class HashTableTest {
     @Test
     public void hashFunTest() {
         int size = 4;
-        HashTable hashTable = new HashTable(size, 1);
+        int step = 1;
+        HashTable hashTable = new HashTable(size, step);
         IntStream.range(1, 10)
                 .boxed()
                 .forEach(x -> {
@@ -27,21 +28,24 @@ public class HashTableTest {
     @Test
     public void hashFunBigValueTest() {
         int size = 4;
-        HashTable hashTable = new HashTable(size, 1);
+        int step = 1;
+        HashTable hashTable = new HashTable(size, step);
         Assertions.assertEquals(3, hashTable.hashFun("10000000000"));
     }
 
     @Test
     public void hashFunNullValueTest() {
         int size = 4;
-        HashTable hashTable = new HashTable(size, 1);
+        int step = 1;
+        HashTable hashTable = new HashTable(size, step);
         Assertions.assertEquals(0, hashTable.hashFun(null));
     }
 
     @Test
     public void putVaryWithSameHashAndEvenSizeOddStepTest() {
         int size = 4;
-        HashTable hashTable = new HashTable(size, 1);
+        int step = 1;
+        HashTable hashTable = new HashTable(size, step);
         Stream.of(1, 5, 9, 12, 16).forEach(x -> hashTable.put(String.valueOf(x)));
         Assertions.assertArrayEquals(Arrays.asList("12", "1", "5", "9").toArray(), hashTable.slots);
     }
@@ -49,7 +53,8 @@ public class HashTableTest {
     @Test
     public void putVaryWithSameHashAndEvenSizeEvenStepTest() {
         int size = 4;
-        HashTable hashTable = new HashTable(size, 2);
+        int step = 2;
+        HashTable hashTable = new HashTable(size, step);
         Stream.of(1, 5, 9, 12, 16).forEach(x -> hashTable.put(String.valueOf(x)));
         Assertions.assertArrayEquals(Arrays.asList(null, "1", null, "5").toArray(), hashTable.slots);
     }
@@ -57,7 +62,8 @@ public class HashTableTest {
     @Test
     public void putVaryWithSameHashAndOddSizeOddStepTest() {
         int size = 5;
-        HashTable hashTable = new HashTable(size, 1);
+        int step = 1;
+        HashTable hashTable = new HashTable(size, step);
         Stream.of(1, 6, 12, 17, 21, 26).forEach(x -> hashTable.put(String.valueOf(x)));
         Assertions.assertArrayEquals(Arrays.asList("6", "12", "17", "21", "1").toArray(), hashTable.slots);
     }
@@ -65,7 +71,8 @@ public class HashTableTest {
     @Test
     public void putVaryWithSameHashAndOddSizeEvenStepTest() {
         int size = 5;
-        HashTable hashTable = new HashTable(size, 2);
+        int step = 2;
+        HashTable hashTable = new HashTable(size, step);
         Stream.of(1, 6, 12, 17, 21, 26).forEach(x -> hashTable.put(String.valueOf(x)));
         Assertions.assertArrayEquals(Arrays.asList("17", "6", "21", "12", "1").toArray(), hashTable.slots);
     }
@@ -73,7 +80,8 @@ public class HashTableTest {
     @Test
     public void putSameTest() {
         int size = 4;
-        HashTable hashTable = new HashTable(size, 1);
+        int step = 1;
+        HashTable hashTable = new HashTable(size, step);
         Stream.of(1, 1).forEach(x -> hashTable.put(String.valueOf(x)));
         Assertions.assertArrayEquals(Arrays.asList(null, "1", null, null).toArray(), hashTable.slots);
     }
@@ -81,7 +89,8 @@ public class HashTableTest {
     @Test
     public void putEvenSizeOddStepVaryTest() {
         int size = 4;
-        HashTable hashTable = new HashTable(size, 1);
+        int step = 1;
+        HashTable hashTable = new HashTable(size, step);
         IntStream.range(1, 2 * size + 1)
                 .boxed()
                 .forEach(x -> hashTable.put(String.valueOf(x)));
@@ -91,7 +100,8 @@ public class HashTableTest {
     @Test
     public void putEvenSizeEvenStepVaryTest() {
         int size = 4;
-        HashTable hashTable = new HashTable(size, 2);
+        int step = 2;
+        HashTable hashTable = new HashTable(size, step);
         IntStream.range(1, 2 * size + 1)
                 .boxed()
                 .forEach(x -> hashTable.put(String.valueOf(x)));
@@ -101,7 +111,8 @@ public class HashTableTest {
     @Test
     public void putOddSizeOddStepVaryTest() {
         int size = 5;
-        HashTable hashTable = new HashTable(size, 1);
+        int step = 1;
+        HashTable hashTable = new HashTable(size, step);
         IntStream.range(1, 2 * size + 1)
                 .boxed()
                 .forEach(x -> hashTable.put(String.valueOf(x)));
@@ -111,7 +122,8 @@ public class HashTableTest {
     @Test
     public void putOddSizeEvenStepVaryTest() {
         int size = 5;
-        HashTable hashTable = new HashTable(size, 2);
+        int step = 2;
+        HashTable hashTable = new HashTable(size, step);
         IntStream.range(1, 2 * size + 1)
                 .boxed()
                 .forEach(x -> hashTable.put(String.valueOf(x)));
@@ -121,7 +133,8 @@ public class HashTableTest {
     @Test
     public void findTest() {
         int size = 4;
-        HashTable hashTable = new HashTable(size, 1);
+        int step = 1;
+        HashTable hashTable = new HashTable(size, step);
         Stream.of(1, 5, 9, 12, 16).forEach(x -> hashTable.put(String.valueOf(x)));
         AtomicInteger i = new AtomicInteger(-2);
         Stream.of(16, 12, 1, 5, 9).forEach(x -> Assertions.assertEquals(i.incrementAndGet() % 4, hashTable.find(String.valueOf(x))));
@@ -129,7 +142,9 @@ public class HashTableTest {
 
     @Test
     public void findEmptyTest() {
-        HashTable hashTable = new HashTable(4, 1);
+        int size = 4;
+        int step = 1;
+        HashTable hashTable = new HashTable(size, step);
         Assertions.assertEquals(-1, hashTable.find("1"));
     }
 }
