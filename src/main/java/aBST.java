69f49839bf14c2a0c73e8d5de5a468533f4eb1cc
Maksim Gusev <aquariusborelias@gmail.com>
2022-07-23 14:09:26 +0300

    lesson04

---
+++ src/main/java/aBST.java
@@ -0,0 +1,54 @@
+class aBST {
+
+    public Integer Tree[]; // массив ключей
+
+    public aBST(int depth) {
+        // правильно рассчитайте размер массива для дерева глубины depth:
+        int tree_size = 0;
+        for (int i = 0; i <= depth; i++) {
+            tree_size += Math.pow(2, i);
+        }
+
+        Tree = new Integer[tree_size];
+        for (int i = 0; i < tree_size; i++) Tree[i] = null;
+    }
+
+    public Integer FindKeyIndex(int key) {
+        if (Tree.length == 0) {
+            return null; // не найден
+        }
+
+        return FindKeyIndex(key, 0);
+    }
+
+    private Integer FindKeyIndex(int key, int index) {
+        if (index > Tree.length - 1) {
+            return null; // не найден
+        }
+
+        if (Tree[index] == null) {
+            return -index;
+        }
+
+        if (Tree[index] == key) {
+            return index;
+        }
+
+        if (key < Tree[index]) {
+            return FindKeyIndex(key, 2 * index + 1);
+        }
+        return FindKeyIndex(key, 2 * index + 2);
+    }
+
+    public int AddKey(int key) {
+        Integer findKeyIndex = FindKeyIndex(key);
+        if (findKeyIndex == null) {
+            return -1;
+        }
+
+        int keyIndex = Math.abs(findKeyIndex);
+        Tree[keyIndex] = key;
+
+        return keyIndex;
+    }
+}
