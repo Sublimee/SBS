4f8f5e42b958792472e063f68fd7c5d88a163256
Maksim Gusev <aquariusborelias@gmail.com>
2022-08-05 18:06:29 +0300

    lesson 10

--- src/main/java/SimpleGraph.java
+++ src/main/java/SimpleGraph.java
@@ -1,10 +1,14 @@
+import java.util.*;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 class Vertex {
     public int Value;
+    public boolean Hit;
 
     public Vertex(int val) {
         Value = val;
+        Hit = false;
     }
 }
 
@@ -77,5 +81,100 @@ class SimpleGraph {
         if (isVertexIndexIncorrect(v1) || isVertexIndexIncorrect(v2)) {
             throw new IndexOutOfBoundsException();
         }
+    }
+
+    Deque<Vertex> stack = new LinkedList<>();
+
+    public ArrayList<Vertex> DepthFirstSearch(int VFrom, int VTo) {
+        init();
+
+        if (!areIndexesValid(VFrom, VTo)) {
+            return new ArrayList<>(stack);
+        }
+
+        if (vertex[VFrom] == null || vertex[VTo] == null) {
+            return new ArrayList<>(stack);
+        }
+
+        vertex[VFrom].Hit = true;
+        stack.push(vertex[VFrom]);
+
+        find(VFrom, VTo);
+
+        ArrayList<Vertex> result = new ArrayList<>(stack);
+        Collections.reverse(result);
+        return result;
+    }
+
+    private void find(int VFrom, int VTo) {
+        Pair nextVertex = getNextVertex(VFrom, VTo);
+        if (nextVertex != null) {
+            stack.push(nextVertex.v);
+            nextVertex.v.Hit = true;
+            if (nextVertex.i == VTo) {
+                return;
+            }
+            find(nextVertex.i, VTo);
+            return;
+        }
+
+        stack.pop();
+        if (stack.isEmpty()) {
+            return;
+        }
+        
+        find(IntStream.range(0, max_vertex)
+                        .boxed()
+                        .filter(i -> vertex[i].equals(stack.getFirst()))
+                        .findFirst()
+                        .orElse(null),
+                VTo
+        );
+    }
+
+    private Pair getNextVertex(int VFrom, int VTo) {
+        List<Integer> adjacentVertexes = IntStream.range(0, max_vertex)
+                .boxed()
+                .filter(i -> m_adjacency[i][VFrom] == 1)
+                .collect(Collectors.toList());
+
+        Integer requiredVertexIndex = adjacentVertexes
+                .stream()
+                .filter(adjacent -> vertex[adjacent].equals(vertex[VTo]))
+                .findFirst().orElse(null);
+
+        if (requiredVertexIndex != null) {
+            return new Pair(requiredVertexIndex, vertex[requiredVertexIndex]);
+        }
+
+        return adjacentVertexes.stream()
+                .map(i -> new Pair(i, vertex[i]))
+                .filter(p -> !p.v.Hit)
+                .findFirst()
+                .orElse(null);
+    }
+
+    private boolean areIndexesValid(int vFrom, int vTo) {
+        if (vFrom < 0 || vTo < 0) {
+            return false;
+        }
+        return vFrom < vertex.length && vTo < vertex.length;
+    }
+
+    private void init() {
+        stack = new ArrayDeque<>();
+        Arrays.stream(vertex)
+                .filter(Objects::nonNull)
+                .forEach(x -> x.Hit = false);
+    }
+}
+
+class Pair {
+    public int i;
+    public Vertex v;
+
+    public Pair(int i, Vertex v) {
+        this.i = i;
+        this.v = v;
     }
 }
