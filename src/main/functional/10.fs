c803752c1dcf06a14939d2f07d6e897da4e7a097
Maxim Gusev <Sublimee@users.noreply.github.com>
2023-03-22 10:14:13 +0300

    Update 10.fs

--- src/main/functional/10.fs
+++ src/main/functional/10.fs
@@ -1,11 +1,7 @@
 type TimeOfDay = { hours: int; minutes: int; f: string }
 
-let evaluate x = function
-    | x when x > 0 -> true
-    | x -> false
-    
-let format = function
-    | (hours, minutes, f) when f = "PM" -> (hours + 12) * 60 + minutes
-    | (hours, minutes, f) -> (hours * 60) + minutes
-    
-let (.>.) x y = evaluate (format(x) - format(y))
+let format x =
+    if x.f = "PM" then (x.hours + 12) * 60 + x.minutes
+    else x.hours * 60 + x.minutes
+
+let (.>.) x y = format(x) > format(y)
