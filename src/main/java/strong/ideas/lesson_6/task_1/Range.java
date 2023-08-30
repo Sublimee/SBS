e4a680e979f8732a5b478cb69e6bac216c8f6b5e
Maksim Gusev <magusev@alfabank.ru>
2023-08-30 07:49:47 +0300

    hw6 first example init

---
+++ src/main/java/strong/ideas/lesson_6/task_1/Range.java
@@ -0,0 +1,34 @@
+package strong.ideas.lesson_6.task_1;
+
+import java.util.Objects;
+
+class Range {
+    int indexFrom;
+    int indexTo;
+
+    public Range(int indexFrom, int indexTo) {
+        this.indexFrom = indexFrom;
+        this.indexTo = indexTo;
+    }
+
+    public int getIndexFrom() {
+        return indexFrom;
+    }
+
+    public int getIndexTo() {
+        return indexTo;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Range range = (Range) o;
+        return indexFrom == range.indexFrom && indexTo == range.indexTo;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(indexFrom, indexTo);
+    }
+}
--- src/test/java/strong/ideas/lesson_6/task_1/ExampleTest.java
+++ src/test/java/strong/ideas/lesson_6/task_1/ExampleTest.java
@@ -1,4 +1,16 @@
 package strong.ideas.lesson_6.task_1;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static strong.ideas.lesson_6.task_1.Example.processArray;
+
 public class ExampleTest {
+
+    @Test
+    void processArrayTest() {
+        int[] example = new int[]{1, 2, 3};
+        Assertions.assertNull(processArray(example));
+    }
+
 }
