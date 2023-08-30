cbd622b25ab296c38167d6026ff00a44207c7d86
Maksim Gusev <magusev@alfabank.ru>
2023-08-30 15:34:20 +0300

    hw6 second example, init

--- src/main/java/strong/ideas/lesson_6/task_2/Example.java
+++ src/main/java/strong/ideas/lesson_6/task_2/Example.java
@@ -1,4 +1,8 @@
 package strong.ideas.lesson_6.task_2;
 
 public class Example {
+
+    public static int[] summarize(int[] arg1, int[] arg2) {
+        return null;
+    }
 }
--- src/test/java/strong/ideas/lesson_6/task_2/ExampleTest.java
+++ src/test/java/strong/ideas/lesson_6/task_2/ExampleTest.java
@@ -1,4 +1,16 @@
 package strong.ideas.lesson_6.task_2;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static strong.ideas.lesson_6.task_2.Example.summarize;
+
 public class ExampleTest {
+
+    @Test
+    void summarizeArraysTest() {
+        int[] arg1 = new int[]{};
+        int[] arg2 = new int[]{};
+        Assertions.assertNull(summarize(arg1, arg2));
+    }
 }
