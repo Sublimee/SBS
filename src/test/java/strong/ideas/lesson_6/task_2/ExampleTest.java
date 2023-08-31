850be955f4a807cfeefdba9fa1391a98bd61c82f
Maksim Gusev <magusev@alfabank.ru>
2023-08-31 08:58:52 +0300

    hw6 second example, step 3

--- src/test/java/strong/ideas/lesson_6/task_2/ExampleTest.java
+++ src/test/java/strong/ideas/lesson_6/task_2/ExampleTest.java
@@ -15,4 +15,12 @@ public class ExampleTest {
         int[] arg2 = new int[]{0};
         Assertions.assertArrayEquals(new int[]{1}, summarize(arg1, arg2));
     }
+
+    @Test
+    @DisplayName("должен сложить 2 числа с одним разрядом без переноса")
+    void summarizeArrays2Test() {
+        int[] arg1 = new int[]{7};
+        int[] arg2 = new int[]{2};
+        Assertions.assertArrayEquals(new int[]{9}, summarize(arg1, arg2));
+    }
 }
