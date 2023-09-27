322fb8572fad95c27bbb849899f770819537a067
Maksim Gusev <magusev@alfabank.ru>
2023-09-27 20:58:13 +0300

    Stack АТД

--- src/main/java/oa1/Stack.java
+++ src/main/java/oa1/Stack.java
@@ -3,17 +3,66 @@ package oa1;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * АТД Stack
+ *
+ * abstract class Stack<T>
+ *
+ *  интерфейс класса, реализующий АТД Stack
+ *  public final int PUSH_NIL = 0;  // push() ещё не вызывалась
+ *  public final int PUSH_OK = 1;   // последняя push() отработала нормально
+ *
+ *  public final int POP_NIL = 0;   // pop() ещё не вызывалась
+ *  public final int POP_OK = 1;    // последняя pop() отработала нормально
+ *  public final int POP_ERR = 2;   // стек пуст
+ *
+ *  public final int PEEK_NIL = 0;  // peek() ещё не вызывалась
+ *  public final int PEEK_OK = 1;   // последняя peek() вернула корректное значение
+ *  public final int PEEK_ERR = 2;  // стек пуст
+ *
+ *  // конструктор
+ *  // постусловие: создан новый пустой стек
+ *  public Stack<T> Stack();
+ *
+ *  // команды:
+ *
+ *  // постусловие: в стек добавлено новое значение
+ *  public void push(T value);
+ *
+ *  // предусловие: стек не пустой;
+ *  // постусловие: из стека удалён верхний элемент
+ *  public void pop();
+ *
+ *  // постусловие: из стека удалятся все значения
+ *  public void clear();
+ *
+ *  // запросы:
+ *
+ *  // предусловие: стек не пустой
+ *  public T peek();
+ *
+ *  public int size();
+ *
+ *  // дополнительные запросы:
+ *
+ *  public int get_push_status(); // возвращает значение PUSH_*
+ *  public int get_pop_status(); // возвращает значение POP_*
+ *  public int get_peek_status(); // возвращает значение PEEK_*
+ */
 public class Stack<T> {
 
     // интерфейс класса, реализующий АТД Stack
-    public final int PUSH_NIL = 0;   // push() ещё не вызывалась
-    public final int PUSH_OK = 1;    // последняя push() отработала нормально
-    public final int POP_NIL = 0;   // push() ещё не вызывалась
+    public final int PUSH_NIL = 0;  // push() ещё не вызывалась
+    public final int PUSH_OK = 1;   // последняя push() отработала нормально
+
+    public final int POP_NIL = 0;   // pop() ещё не вызывалась
     public final int POP_OK = 1;    // последняя pop() отработала нормально
     public final int POP_ERR = 2;   // стек пуст
-    public final int PEEK_NIL = 0;  // push() ещё не вызывалась
+
+    public final int PEEK_NIL = 0;  // peek() ещё не вызывалась
     public final int PEEK_OK = 1;   // последняя peek() вернула корректное значение
     public final int PEEK_ERR = 2;  // стек пуст
+
     // скрытые поля
     private List<T> stack;          // основное хранилище стека
     private int push_status;        // статус запроса push()
@@ -49,7 +98,7 @@ public class Stack<T> {
     public void clear() {
         stack = new ArrayList<>(); // пустой список/стек
 
-        // начальные статусы для предусловий push(), peek() и pop()
+        // начальные статусы для push() и предусловий peek() и pop()
         push_status = PUSH_NIL;
         peek_status = PEEK_NIL;
         pop_status = POP_NIL;
