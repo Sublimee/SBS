e6f319e5cd2545695d02f59defa98ca9cf867ef5
Sublimee <aquariusborelias@gmail.com>
2024-11-02 17:24:25 +0300

    sql_18

---
+++ src/main/SQL/18.sql
@@ -0,0 +1,65 @@
+-- 1
+SELECT D.dwarf_id,
+       D.name AS dwarf_name,
+       D.age,
+       D.profession,
+       S.squad_id,
+       S.name AS squad_name,
+       S.mission
+FROM Dwarves D
+         INNER JOIN Squads S
+                    ON S.squad_id = D.squad_id;
+
+-- 2
+SELECT *
+FROM Dwarves
+WHERE profession = 'miner'
+  AND squad_id IS NUll;
+
+--3
+SELECT *
+FROM Tasks
+WHERE status = 'pending'
+  AND priority = (SELECT MAX(priority) FROM Tasks WHERE status = 'pending');
+
+--4
+SELECT D.dwarf_id,
+       D.name,
+       D.age,
+       D.profession,
+       COUNT(*) AS item_count
+FROM Dwarves D
+         INNER JOIN
+     Items I ON I.owner_id = D.dwarf_id
+GROUP BY D.dwarf_id, D.name, D.age, D.profession;
+
+--5
+SELECT S.squad_id,
+       S.name            AS squad_name,
+       COUNT(D.dwarf_id) AS dwarf_count
+FROM Dwarves D
+         RIGHT JOIN
+     Squads S ON S.squad_id = D.squad_id
+GROUP BY S.squad_id, S.name;
+
+--6
+WITH profession_tasks AS (SELECT D.profession,
+                                 COUNT(*) AS incomplete_tasks
+                          FROM Dwarves D
+                                   INNER JOIN
+                               Tasks T ON D.dwarf_id = T.assigned_to
+                          WHERE T.status = 'pending'
+                             OR T.status = 'in_progress'
+                          GROUP BY D.dwarf_id, D.profession),
+     profession_totals AS (SELECT profession,
+                                  SUM(incomplete_tasks) AS total_incomplete_tasks
+                           FROM profession_tasks
+                           GROUP BY profession)
+
+SELECT profession,
+       total_incomplete_tasks
+FROM profession_totals
+WHERE total_incomplete_tasks = (SELECT MAX(total_incomplete_tasks)
+                                FROM profession_totals);
+
+--7
