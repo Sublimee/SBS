163474b5bbad18485d42b211f3e5f3e400a16ae2
Sublimee <aquariusborelias@gmail.com>
2025-12-13 15:14:42 +0300

    algo_py_1

--- python_algo/tests/task1.py
+++ python_algo/tests/task1.py
@@ -113,18 +113,3 @@ def to_array(ll: LinkedList):
         node = node.next
     return result
 
-def sum_linked_lists(ll1: LinkedList, ll2: LinkedList) -> LinkedList | None:
-    if ll1 is None or ll2 is None or ll1.len() != ll2.len():
-        return None
-
-    result = LinkedList()
-    current1 = ll1.head
-    current2 = ll2.head
-
-    while current1 is not None:
-        result.add_in_tail(Node(current1.value + current2.value))
-        current1 = current1.next
-        current2 = current2.next
-
-    return result
-
